7acc2660a5b10655c8964a741a3ba7afc848b4a8
igor349 <igorsvidunovic@gmail.com>
2021-03-30 10:11:23 +0300

    creating main logic on the server

--- backend_nodejs/index.js
+++ backend_nodejs/index.js
@@ -1,4 +1,9 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const db = require('./db/data.json');
+
 
 http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,5 +14,99 @@ http.createServer((req, res) => {
     if (req.method == 'GET') {
         res.end('the server is running on port 3500 through a nodeJS');
     }
+    else {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            let params = JSON.parse(body);
+            switch (params.action) {
+                case 1:
+                    console.log('action 1');
+                    console.log(chek1);
+                    if(chek1){
+                        res.end(JSON.stringify(filterNumberBack(db, 'id')));
+                        chek1=false;
+                        break;
+                    }else {
+                        res.end(JSON.stringify(filterNumber(db, 'id')));
+                        chek1=true
+                        break;
+                    }
+                case 2:
+                    console.log('action 2');
+                    if(chek1){
+                        res.end(JSON.stringify(filterNumber(db, 'number')));
+                        chek1=false;
+                        break;
+                    }else {
+                        res.end(JSON.stringify(filterNumberBack(db, 'number')));
+                        chek1=true
+                        break;
+                    }
+                case 3:
+                    console.log('action 3');
+                    if(chek1){
+                        res.end(JSON.stringify(filterString(db, 'name')));
+                        chek1=false;
+                        break;
+                    }else {
+                        res.end(JSON.stringify(filterStringBack(db, 'name')));
+                        chek1=true
+                        break;
+                    }
+                    break;
+                case 4:
+                    console.log('action 4');
+                    if(chek1){
+                        res.end(JSON.stringify(filterString(db, 'country')));
+                        chek1=false;
+                        break;
+                    }else {
+                        res.end(JSON.stringify(filterStringBack(db, 'country')));
+                        chek1=true
+                        break;
+                    }
+                    break;
+                case 5:
+                    console.log('action 5');
+                    if(chek1){
+                        res.end(JSON.stringify(filterNumber(db, 'miss')));
+                        chek1=false;
+                        break;
+                    }else {
+                        res.end(JSON.stringify(filterNumberBack(db, 'miss')));
+                        chek1=true
+                        break;
+                    }
+                case 6:
+                    console.log('action 6');
+                    if(chek1){
+                        res.end(JSON.stringify(filterNumber(db, 'number')));
+                        chek1=false;
+                        break;
+                    }else {
+                        res.end(JSON.stringify(filterNumberBack(db, 'number')));
+                        chek1=true
+                        break;
+                    }
+            }
+        });
+    }
 
 }).listen(3500);
+
+let chek1= true;
+const filterNumber = (arr, arg) => {
+    return arr.sort((a, b) => a[arg] - b[arg]);
+}
+const filterNumberBack = (arr, arg) => {
+    return arr.sort((a, b) => b[arg] - a[arg]);
+}
+const filterString = (arr, arg) => {
+    return arr.sort((a, b) => (a[arg] < b[arg]) ? -1 : 1);
+}
+const filterStringBack = (arr, arg) => {
+    return arr.sort((a, b) => (a[arg] > b[arg]) ? -1 : 1);
+}
--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -1,8 +1,9 @@
-import {players} from '../data/playersData';
 import {Table} from 'react-bootstrap';
 import React, {useState} from 'react';
 import TableBody from './TableBody';
 import Search from './Search';
+import env from '../env.json';
+import {players} from './data'
 
 
 export default function TableElem() {
@@ -13,66 +14,41 @@ export default function TableElem() {
         let input = event.target.value.toLowerCase();
         setState([...players.filter(item => item['name'].toLowerCase().includes(input))]);
     }
-
-    const filterNumber = (arr, arg) => {
-        return arr.sort((a, b) => a[arg] - b[arg]);
+    function requestToServer(action) {
+        fetch(env.urlBack, {
+            method: 'POST',
+            header: {
+                'Content-type': 'application/x-www-form-urlencoded'
+            },
+            body: JSON.stringify(action)
+        })
+            .then(res => res.json())
+            .then(res => {
+                setState(res)
+            })
     }
-    const filterNumberBack = (arr, arg) => {
-        return arr.sort((a, b) => b[arg] - a[arg]);
-    }
-    const filterString = (arr, arg) => {
-        return arr.sort((a, b) => (a[arg] < b[arg]) ? -1 : 1);
-    }
-    const filterStringBack = (arr, arg) => {
-        return arr.sort((a, b) => (a[arg] > b[arg]) ? -1 : 1);
-    }
-
     function clickPosition() {
-        if (check % 2 === 0) {
-            setState([...filterNumberBack(state, 'id')]);
-        } else {
-            setState([...filterNumber(state, 'id')]);
-        }
+        requestToServer({action:1})
+
+    }
+    function clickNumber() {
+        requestToServer({action:2})
     }
 
     function clickName() {
-        if (check % 2 === 0) {
-            setState([...filterString(state, 'name')]);
-        } else {
-            setState([...filterStringBack(state, 'name')]);
-        }
+        requestToServer({action:3})
     }
 
     function clickCountry() {
-        if (check % 2 === 0) {
-            setState([...filterString(state, 'country')]);
-        } else {
-            setState([...filterStringBack(state, 'country')]);
-        }
-    }
-
-    function clickNumber() {
-        if (check % 2 === 0) {
-            setState([...filterNumber(state, 'number')]);
-        } else {
-            setState([...filterNumberBack(state, 'number')]);
-        }
+        requestToServer({action:4})
     }
 
     function clickMiss() {
-        if (check % 2 === 0) {
-            setState([...filterNumber(state, 'miss')]);
-        } else {
-            setState([...filterNumberBack(state, 'miss')]);
-        }
+        requestToServer({action:5})
     }
 
     function clickShooting() {
-        if (check % 2 === 0) {
-            setState([...filterNumber(state, 'timeShoots')]);
-        } else {
-            setState([...filterNumberBack(state, 'timeShoots')]);
-        }
+        requestToServer({action:6})
     }
 
     return (
@@ -80,7 +56,7 @@ export default function TableElem() {
             <Search searchMember={searchMembers}/>
             <Table striped bordered hover className="text-center table-secondary">
                 <thead>
-                <tr onClick={() => setCheck(check + 1)}>
+                <tr>
                     <th onClick={clickPosition}>Position</th>
                     <th onClick={clickNumber}>Number</th>
                     <th onClick={clickName} style={{width: '40%'}}>Name</th>
